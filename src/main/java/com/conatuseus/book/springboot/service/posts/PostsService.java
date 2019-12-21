b2e7db7cf2ab52f0676c2e67cd0f73b90ca4f50b
MK S <conatuseus@gmail.com>
2019-12-22 00:20:24 +0900

    게시글 등록, 목록조회 기능 구현(~p.151)

--- src/main/java/com/conatuseus/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/conatuseus/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.conatuseus.book.springboot.service.posts;
 
 import com.conatuseus.book.springboot.domain.posts.Posts;
 import com.conatuseus.book.springboot.domain.posts.PostsRepository;
+import com.conatuseus.book.springboot.web.dto.PostsListResponseDto;
 import com.conatuseus.book.springboot.web.dto.PostsResponseDto;
 import com.conatuseus.book.springboot.web.dto.PostsSaveRequestDto;
 import com.conatuseus.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
             .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
     }
 }

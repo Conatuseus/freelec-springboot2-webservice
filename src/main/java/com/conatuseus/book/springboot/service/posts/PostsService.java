3e30e5e59c72cf0c2593e04f00e67c88c990ff42
MK S <conatuseus@gmail.com>
2019-12-21 23:05:14 +0900

    JPA Auditing 기능 추가
    웹 콘솔 옵션 활성화
    JPA Auditing 어노테이션 추가
    BaseTimeEntity 구현
    Posts가 BaseTimeEntity 상속받도록 수정
    시간에 대한 기능 확인을 위한 테스트 코드 추가

--- src/main/java/com/conatuseus/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/conatuseus/book/springboot/service/posts/PostsService.java
@@ -27,7 +27,6 @@ public class PostsService {
             .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
-
         return id;
     }
 
